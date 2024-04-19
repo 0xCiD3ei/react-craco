da5a8975f6be6d904390c40372d8500f52dc1bc6
Snake <tiennd@northstudio.vn>
2024-04-19 13:04:30 +0700

    :wrench: config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+    'prettier'
+  ],
+  ignorePatterns: ['.eslintrc.cjs', '*.config'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier', 'unused-imports'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: true,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ],
+    'no-unused-vars': ['warn',
+      { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }
+    ],
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'varsIgnorePattern': '^_',
+        'args': 'after-used',
+        'argsIgnorePattern': '^_'
+      }
+    ]
+    
+  }
+};
